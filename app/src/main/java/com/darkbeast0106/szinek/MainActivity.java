59783c3d9d70a900bd680f1fc26b72a3a144da41
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-16 09:39:53 +0200

    Véletlen szín generáló alkalmazás

--- app/src/main/java/com/darkbeast0106/szinek/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/szinek/MainActivity.java
@@ -2,14 +2,19 @@ package com.darkbeast0106.szinek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout layout;
     private TextView textSzin;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = rnd.nextInt(256);
+                int green = rnd.nextInt(256);
+                int blue = rnd.nextInt(256);
+                layout.setBackgroundColor(Color.rgb(red, green, blue));
+                String szinSzoveges = String.format("(%d, %d, %d)", red, green, blue);
+                textSzin.setText(szinSzoveges);
+                if ((red + green + blue) > 450){
+                    textSzin.setTextColor(Color.BLACK);
+                } else {
+                    textSzin.setTextColor(Color.WHITE);
+                }
+            }
+        });
     }
 
-    private void init(){
+    private void init() {
         layout = findViewById(R.id.layout);
         textSzin = findViewById(R.id.textSzin);
+        rnd = new Random();
     }
 }
